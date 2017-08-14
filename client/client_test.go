c88a9d71482299814230c925725f98c3cfa656a3
Adam Jones <adam@digirati.co.nz>
2017-08-15 11:04:01 +1200

    Add some functional tests

--- client/client_test.go
+++ client/client_test.go
@@ -2,6 +2,7 @@ package client_test
 
 import (
 	"net/http"
+	"net/http/httptest"
 	"testing"
 
 	"github.com/dailyburn/sailthru-go/client"
@@ -10,11 +11,39 @@ import (
 
 func mockClient(url string) *client.Client {
 	client.BaseURL = url
-	return &client.Client{HTTPClient: http.DefaultClient}
+	c := client.NewClient("key", "secret")
+	return c
 }
 
 // Unit Tests
 
 func TestClientDefaultHost(t *testing.T) {
 	assert.Equal(t, "https://api.sailthru.com", client.BaseURL)
+}
+
+// Functional Tests
+
+func TestDefaultHeaders(t *testing.T) {
+	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		assert.Equal(t, "application/x-www-form-urlencoded", r.Header.Get("Content-Type"))
+		assert.Equal(t, "72", r.Header.Get("Content-Length"))
+	}))
+	defer server.Close()
+	err := mockClient(server.URL).Post("", "")
+	assert.NoError(t, err)
+}
+
+func TestPost(t *testing.T) {
+	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		assert.Equal(t, "POST", r.Method)
+	}))
+	defer server.Close()
+	err := mockClient(server.URL).Post("", "")
+	assert.NoError(t, err)
+}
+
+func TestInvalidJSONPayload(t *testing.T) {
+	payload := make(chan int)
+	err := mockClient("").Post("", payload)
+	assert.Error(t, err)
 }
